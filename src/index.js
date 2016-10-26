bdfa5d3daa834d0537ed7db60c21264b44e969b7
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2016-10-26 20:54:09 +0200

    Import CV files after base css are in place

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CV from './CV'
+
 import './normalize.css'
 import './base.css'
 import './print.css'
 import './fonts/font-awesome.min.css'
+
+import CV from './CV'
 
 ReactDOM.render(
   <CV />,
