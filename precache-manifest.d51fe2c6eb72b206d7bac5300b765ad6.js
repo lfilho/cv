e549425460d8eebed31a2e596f6c1c0816a359b4
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2019-06-17 19:45:12 -0700

    Updates

---
+++ precache-manifest.d51fe2c6eb72b206d7bac5300b765ad6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90dc72d01e4b7fb7471f0cc4a9ff6f9b",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "d5a339baa21ddeee00d2",
+    "url": "/cv/static/css/main.ac340f1b.chunk.css"
+  },
+  {
+    "revision": "5800b0bb0b8fb338736d",
+    "url": "/cv/static/js/2.3767cfa3.chunk.js"
+  },
+  {
+    "revision": "d5a339baa21ddeee00d2",
+    "url": "/cv/static/js/main.4de1fbf1.chunk.js"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cv/precache-manifest.87923b03f03a17f08baa686224cbe5a6.js"
+  "/cv/precache-manifest.d51fe2c6eb72b206d7bac5300b765ad6.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/cv/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cv/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
